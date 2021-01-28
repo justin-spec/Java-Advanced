47a2f2f0256b567d2101d55f273aa3f5abe6a18c
justin-spec <justinchen.ken@protonmail.com>
2021-01-27 19:16:12 -0800

    OTP is functional and complete with swing

--- classwork/w01w_OTPGen/OTP.java
+++ classwork/w01w_OTPGen/OTP.java
@@ -1,8 +1,13 @@
 package w01w_OTPGen;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 import java.util.Random;
 
 // OTP or two factor identification
@@ -11,6 +16,33 @@ public class OTP extends JFrame implements ActionListener {
 	
 	JButton newOTP;
 	JLabel resultLabel;
+	
+	OTP (){
+		// creates button and label
+		newOTP = new JButton("Refresh!");
+		resultLabel = new JLabel("Your OTP Code");
+		newOTP.addActionListener(this); // do some action when clicked
+		
+		// creates Border for layout(?)
+		setLayout(new BorderLayout());
+		
+		// 'this' keyword is the JFrame we're working on
+		// this.add
+		
+		// places buttons on border
+		add(newOTP, BorderLayout.SOUTH);
+		add(resultLabel, BorderLayout.CENTER);
+		
+		// exit location
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// where it opens
+		setLocation(250, 100);
+		// shows on screen
+		setVisible(true);
+		// size dimensions
+		setSize(600, 350);
+	}
 	
 	public static char[] generate(int len) {
 		System.out.println("Generating OTP Number.");
@@ -27,17 +59,21 @@ public class OTP extends JFrame implements ActionListener {
 		return otp;
 	}
 	
-	public static void main(String[] args) {
-		// creates a string of 6 random numbers as characters in an array
-		System.out.println(OTP.generate(6));
-
-	}
-	
 	//arg0
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		int length = 6;
+		// creates a string of 6 random numbers as characters in an array
+		System.out.println(OTP.generate(length));
+		// changes text of label to OTP generate
+		resultLabel.setText(String.valueOf(OTP.generate(length)));
+	}
+	
+	public static void main(String[] args) {
+		// opens swing
+		new OTP();
+
 	}
 
 }
